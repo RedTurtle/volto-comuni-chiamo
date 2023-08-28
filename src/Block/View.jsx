412a9e5b2de1f5ba025eab98e996c31b679e8702
Mauro Amico <mauro.amico@gmail.com>
2023-08-28 18:01:00 +0200

    fix: comunichiamo 0.4.0

--- src/Block/View.jsx
+++ src/Block/View.jsx
@@ -3,17 +3,37 @@ import PropTypes from 'prop-types';
 import { CC_PALETTE } from 'volto-comuni-chiamo/Block/colors';
 
 const View = ({ data, properties, id, path }) => {
-  const [scriptLoaded, setScriptLoaded] = React.useState(null);
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.src =
-      'https://cdn-embed.comuni-chiamo.com/test/0.3.1-beta/js/main.min.js';
-    script.async = true;
+  const src =
+    'https://cdn-embed.comuni-chiamo.com/test/0.4.0-beta/js/main.min.js';
 
+  // src: https://stackoverflow.com/a/28002292
+  const getScript = (source, callback) => {
+    var script = document.createElement('script');
+    var prior = document.getElementsByTagName('script')[0];
+    script.async = 1;
+
+    script.onload = script.onreadystatechange = (_, isAbort) => {
+      if (
+        isAbort ||
+        !script.readyState ||
+        /loaded|complete/.test(script.readyState)
+      ) {
+        script.onload = script.onreadystatechange = null;
+        script = undefined;
+        if (!isAbort) if (callback) callback();
+      }
+    };
+
+    script.src = source;
+    prior.parentNode.insertBefore(script, prior);
+  };
+
+  useEffect(() => {
     const checkColor = (color) => {
       const colorSelect = CC_PALETTE.filter((el) => el.name === color);
       return colorSelect[0].code;
     };
+
     const ccWidgetReportingConf = {
       targetId: 'comunichiamo',
       apiKey: data.keyWidget,
@@ -21,22 +41,22 @@ const View = ({ data, properties, id, path }) => {
         primaryColor:
           data.colorWidget ?? checkColor(data.color || CC_PALETTE[0].name),
       },
+      autoInit: false,
     };
     window.ccWidgetReportingConf = ccWidgetReportingConf;
-    document.body.appendChild(script);
-    setScriptLoaded(script);
 
-    return () => {
-      scriptLoaded && document.body.removeChild(scriptLoaded);
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+    getScript(src, () => {
+      window.ccWidgetReporting.init();
+    });
+  }, [data.color, data.colorWidget, data.keyWidget]);
 
   return (
-    <div className="block comuni-chiamo">
-      {data.title && <h2 className="mb-4">{data.title}</h2>}
-      <div id="comunichiamo"></div>
-    </div>
+    <>
+      <div className="block comuni-chiamo">
+        {data.title && <h2 className="mb-4">{data.title}</h2>}
+        <div id="comunichiamo"></div>
+      </div>
+    </>
   );
 };
 
